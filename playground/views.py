41d50e34b9664dcfe8813088219f5d4ac8ea4a45
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-23 18:54:23 +0100

    Managers and QuerySets

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from store.models import Product
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    query_set.filter()
+    query_set = Product.objects.count()
 
     return render(request, 'hello.html', {'name': 'Mosh'})
