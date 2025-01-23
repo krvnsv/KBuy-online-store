5e732de2e51a0f166c89d34b521c05b52fdc789b
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-23 19:02:20 +0100

    Retrieving Objects

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 def say_hello(request):
-    query_set = Product.objects.count()
+    # None
+    exists = Product.objects.filter(pk=0).exists()
 
     return render(request, 'hello.html', {'name': 'Mosh'})
