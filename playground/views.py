5cf5d17aa63403e2c0335672c61ab183902514e2
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-24 15:30:15 +0100

    Referencing fields using F objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 def say_hello(request):
-    # Products: inventory < 10 AND prive < 20
-    queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+    # Products: inventory = price
+    queryset = Product.objects.filter(inventory=F('collection__id'))
 
     return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
