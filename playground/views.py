c485436c10b2cd88021c26767a97656fd2879079
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-24 22:18:56 +0100

    Deffering fields

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from store.models import Product, OrderItem
 
 def say_hello(request):
-    query_set = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    query_set = Product.objects.defer('description')
     
     return render(request, 'hello.html', {'name': 'Mosh', 'products': query_set})
