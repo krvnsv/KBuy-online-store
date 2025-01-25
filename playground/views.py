a6bb49c08b40284eb1dfa525c7941a1a1c4a7f6d
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-25 20:48:08 +0100

    Calling database functions

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.db.models import Value, F
+from django.db.models import Value, F, Func
+from django.db.models.functions import Concat
 from store.models import Product, Customer
 
 def say_hello(request):
-    query_set = Customer.objects.annotate(new_id=F('id') + 1)
+    query_set = Customer.objects.annotate(full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
     
+    query_set = Customer.objects.annotate(full_name=Concat('first_name', Value(' '), 'last_name'))
+
     return render(request, 'hello.html', {'name': 'Mosh', 'result': list(query_set)})
