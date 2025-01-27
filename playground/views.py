089a7c4b8d07f465536683f519ad93e425d9d83b
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-27 16:28:42 +0100

    Custom managers

--- playground/views.py
+++ playground/views.py
@@ -4,12 +4,6 @@ from store.models import Product
 from tags.models import TaggedItem
 
 def say_hello(request):
-    content_type = ContentType.objects.get_for_model(Product)
-
-    query_set = TaggedItem.objects \
-        .select_related('tag') \
-        .filter(
-            content_type=content_type, 
-            object_id=1)
+    TaggedItem.objects.get_tags_for(Product, 1)
     
     return render(request, 'hello.html', {'name': 'Mosh', 'tags': list(query_set)})
