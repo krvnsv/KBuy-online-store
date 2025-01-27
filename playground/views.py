41f98bed1f3eed334d070abe586c62836c55f50e
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-28 00:12:09 +0100

    Transactions

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from django.db import transaction
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product, Collection
+from store.models import Order, OrderItem, Product, Collection
 from tags.models import TaggedItem
 
 def say_hello(request):
-    collection = Collection(pk=11)
-    # collection.delete()
 
-    Collection.objects.filter(id__gt=10).delete()
+    # ...
+
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = -1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
+
 
     return render(request, 'hello.html', {'name': 'Kirill'})
