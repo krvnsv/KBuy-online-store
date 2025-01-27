6abae20a0217385fb2d2b12d5dd33f5c82d83e36
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-28 00:21:34 +0100

    Executing raw sql queries

--- playground/views.py
+++ playground/views.py
@@ -5,20 +5,6 @@ from store.models import Order, OrderItem, Product, Collection
 from tags.models import TaggedItem
 
 def say_hello(request):
+    query_set = Product.objects.raw('SELECT id, title FROM store_product')
 
-    # ...
-
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 1
-        order.save()
-
-        item = OrderItem()
-        item.order = order
-        item.product_id = -1
-        item.quantity = 1
-        item.unit_price = 10
-        item.save()
-
-
-    return render(request, 'hello.html', {'name': 'Kirill'})
+    return render(request, 'hello.html', {'name': 'Kirill', 'result': list(query_set)})
