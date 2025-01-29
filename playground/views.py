edc0729b028d4bf7641cb39b3084ca20a98b00c2
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-29 16:24:17 +0100

    Setting up the admin site

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-from django.db import transaction
-from django.contrib.contenttypes.models import ContentType
-from store.models import Order, OrderItem, Product, Collection
-from tags.models import TaggedItem
 
 def say_hello(request):
-    query_set = Product.objects.raw('SELECT id, title FROM store_product')
-
+    
     return render(request, 'hello.html', {'name': 'Kirill', 'result': list(query_set)})
