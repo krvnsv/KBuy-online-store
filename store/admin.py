5705ec0eea4f709fa1bf132c9f5ab78d52cdc261
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-31 14:08:34 +0100

    Selecting related objects

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,13 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title',]
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -22,3 +26,8 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 admin.site.register(models.Collection)
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
