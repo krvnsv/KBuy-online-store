2d37720d6efd225a826239dc614d75a9dc2603b1
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-02-05 00:27:24 +0100

    Data validation

--- store/serializers.py
+++ store/serializers.py
@@ -13,7 +13,8 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
 
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
     
     
     def calculate_tax(self, product: Product):
