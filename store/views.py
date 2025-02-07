40d1f1cc594db3ed68eddffd15b638078c2b4d1f
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-02-07 23:55:46 +0100

    Routers

--- store/views.py
+++ store/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework import status
-from .models import Product, Collection
+from .models import OrderItem, Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
@@ -19,13 +19,17 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item'},
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        return super().destroy(request, *args, **kwargs)
+
+    # def delete(self, request, pk):
+    #     product = get_object_or_404(Product, pk=pk)
+    #     product.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
         
 
 class CollectionViewSet(ModelViewSet):
