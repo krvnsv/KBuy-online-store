3a52505b7d9fbd8eacc18b5209708ca96d160cfb
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-02-13 22:53:13 +0100

    Getting the current user

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,13 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateClass
+from djoser.serializers import UserSerializer as BaseUserSerializer
 from rest_framework import serializers
 
 class UserCreateSerializer(BaseUserCreateClass):
     class Meta(BaseUserCreateClass.Meta):
         fields = ['id', 'username', 'password', 
                   'email', 'first_name', 'last_name']
+        
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
