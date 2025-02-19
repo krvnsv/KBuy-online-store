da24b5291933197f6e7ddbd51701967e74d700ad
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-02-20 00:33:48 +0100

    Setting up the project

--- storefront/settings.py
+++ storefront/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
+        'NAME': 'storefront3',
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': 'kuro4ka04'
