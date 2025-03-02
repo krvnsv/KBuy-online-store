cf0a10bc90a105d243f415df23939ca50f7a92b2
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-03-02 22:44:18 +0100

    Skipping tests

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -2,14 +2,10 @@ from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
 
+@pytest.mark.django_db
 class TestCreateCollection:
-    @pytest.mark.django_db
+    @pytest.mark.skip
     def test_if_user_is_anonymous_return_401(self):
-        # Arrange
-
-        # Act
         client = APIClient()
         response = client.post('/store/collections/', { 'title': 'a'})
-
-        # Assertion
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
