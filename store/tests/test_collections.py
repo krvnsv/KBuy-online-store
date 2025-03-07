9e0b5d97b498e15512f163b776a1e592c178fa8b
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-03-07 22:54:41 +0100

    Fix fixtures

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,10 +1,8 @@
 from store.models import Collection, Product
-from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
 from model_bakery import baker
 
-from store.tests.conftest import authenticate
 
 @pytest.fixture
 def create_collection(api_client):
@@ -15,33 +13,31 @@ def create_collection(api_client):
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_return_401(self, create_collection):
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
         response = create_collection({'title': 'a'})
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-
-    def test_if_user_is_not_admin_return_403(self, authenticate, create_collection):
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
         authenticate()
 
-        response = create_collection({ 'title': 'a'})
-        
+        response = create_collection({'title': 'a'})
+
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-
-    def test_if_data_is_invalid_return_400(self, api_client, create_collection):
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
         authenticate(is_staff=True)
-        response = create_collection({ 'title': ''})
-        
+
+        response = create_collection({'title': ''})
+
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-
-    def test_if_data_is_valid_return_201(self, api_client, create_collection):
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
         authenticate(is_staff=True)
-        
-        response = create_collection({ 'title': 'a'})
-        
+
+        response = create_collection({'title': 'a'})
+
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
 
