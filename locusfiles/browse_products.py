ffd16b736f4899499652a03e5baae1deb97bf8e6
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-03-10 12:20:10 +0100

    Running a performance test

--- locusfiles/browse_products.py
+++ locusfiles/browse_products.py
@@ -7,7 +7,6 @@ class WebsiteUser(HttpUser):
     # Viewing products
     @task(2)
     def view_products(self):
-        print('View products')
         collection_id = randint(2, 6)
         self.client.get(
             f'/store/products/?collection_id={collection_id}', 
@@ -17,7 +16,6 @@ class WebsiteUser(HttpUser):
     # Viewing product details
     @task(4)
     def view_product(self):
-        print('View product details')
         product_id = randint(1, 1000)
         self.client.get(
             f'/store/products/{product_id}', 
@@ -27,7 +25,6 @@ class WebsiteUser(HttpUser):
     # Add product to cart
     @task(1)
     def add_to_cart(self):
-        print('Add to cart')
         product_id = randint(1, 10)
         self.client.post(
             f'/store/carts/{self.cart_id}/items/',
