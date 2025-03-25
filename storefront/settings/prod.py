338c1afad7026512b1eda4d9543b35d2a62558bd
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-03-26 00:10:11 +0100

    Provisioning mysql database

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,6 +1,6 @@
 import os
-
 from storefront.settings.dev import SECRET_KEY
+import dj_database_url
 from .common import *
 
 DEBUG = False
@@ -8,3 +8,7 @@ DEBUG = False
 SECRET_KEY = os.env['SECRET_KEY']
 
 ALLOWED_HOSTS = ['kbuy-prod-10f4d2f1e438.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
